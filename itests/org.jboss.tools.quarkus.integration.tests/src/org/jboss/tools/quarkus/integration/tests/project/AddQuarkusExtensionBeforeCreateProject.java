1800ad79da523f5a8c6fe244344cc20a98217547
olkornii <olkornii@redhat.com>
2020-05-18 09:38:16 +0200

    some changes
    Signed-off-by: olkornii <olkornii@redhat.com>

--- itests/org.jboss.tools.quarkus.integration.tests/src/org/jboss/tools/quarkus/integration/tests/project/AddQuarkusExtensionBeforeCreateProject.java
+++ itests/org.jboss.tools.quarkus.integration.tests/src/org/jboss/tools/quarkus/integration/tests/project/AddQuarkusExtensionBeforeCreateProject.java
@@ -17,12 +17,22 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.reddeer.common.wait.TimePeriod;
+import org.eclipse.reddeer.core.handler.WidgetHandler;
 import org.eclipse.reddeer.eclipse.ui.navigator.resources.ProjectExplorer;
 import org.eclipse.reddeer.jface.text.contentassist.ContentAssistant;
 import org.eclipse.reddeer.junit.runner.RedDeerSuite;
 import org.eclipse.reddeer.requirements.openperspective.OpenPerspectiveRequirement.OpenPerspective;
 import org.eclipse.reddeer.workbench.impl.editor.TextEditor;
 import org.eclipse.reddeer.workbench.impl.shell.WorkbenchShell;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Widget;
+import org.eclipse.reddeer.common.util.Display;
+import org.eclipse.reddeer.common.util.ResultRunnable;
+import org.eclipse.reddeer.swt.impl.list.DefaultList;
+import org.eclipse.reddeer.swt.impl.styledtext.DefaultStyledText;
+import org.jboss.tools.quarkus.reddeer.common.QuarkusLabels;
 import org.jboss.tools.quarkus.reddeer.common.QuarkusLabels.TextLabels;
 import org.jboss.tools.quarkus.reddeer.perspective.QuarkusPerspective;
 import org.jboss.tools.quarkus.reddeer.wizard.CodeProjectTypeWizardPage;
@@ -65,8 +75,13 @@ public class AddQuarkusExtensionBeforeCreateProject extends AbstractQuarkusTestM
 
 		new CodeProjectExtensionsWizardPage(qw).selectCategory("Cloud");
 		new CodeProjectExtensionsWizardPage(qw).selectExtension("Kubernetes"); // w8 for fix
-																				// https://github.com/jbosstools/jbosstools-quarkus/blob/ad779a37149cff5bf4efda86a3468ba54d1cc215/test-framework/org.jboss.tools.quarkus.reddeer/src/org/jboss/tools/quarkus/reddeer/wizard/CodeProjectExtensionsWizardPage.java#L61
-
+		String[] my_extensions = new CodeProjectExtensionsWizardPage(qw).getExtensions();
+		List<String> list_my_extensions = new ArrayList<>(Arrays.asList(my_extensions));
+		int index_of_kubernetes = list_my_extensions.indexOf("Kubernetes");
+		
+		
+		click(index_of_kubernetes);
+		
 		qw.finish(TimePeriod.VERY_LONG);
 
 		assertTrue(new ProjectExplorer().containsProject(PROJECT_NAME));
@@ -85,6 +100,37 @@ public class AddQuarkusExtensionBeforeCreateProject extends AbstractQuarkusTestM
 			AbstractContentAssistantTest.checkProposal(ca, component + proposalForChoose);
 		}
 
+	}
+	
+	public static void click(final int offset) {
+		Event e = createEvent();
+		Widget swtWidget = new DefaultList(QuarkusLabels.TextLabels.EXTENSIONS_NAMES).getSWTWidget(); // mam swtWidget meho listu, kde mam najit "Kubernetes" a kliknout.
+		Point p = new DefaultList(QuarkusLabels.TextLabels.EXTENSIONS_NAMES).getSWTWidget().getLocation();
+		e.x = p.x;
+		e.y = p.y;
+		WidgetHandler.getInstance().notifyWidget(SWT.MouseDoubleClick, e, new DefaultList(QuarkusLabels.TextLabels.EXTENSIONS_NAMES).getSWTWidget());
+	}
+
+//	 returns point (x,y) of offset in this StyledText widget.
+//	private Point getLocationAtOffset(final int offset) {
+//		return Display.syncExec(new ResultRunnable<Point>() {
+////
+//			@Override
+//			public Point run() {
+//				return new DefaultList(QuarkusLabels.TextLabels.EXTENSIONS_NAMES).getSWTWidget().getLocationAtOffset(offset);
+////				swtWidget.
+//			}
+//		});
+//	}
+
+	private static Event createEvent() {
+		Event event = new Event();
+		event.time = (int) System.currentTimeMillis();
+		event.widget = new DefaultList(QuarkusLabels.TextLabels.EXTENSIONS_NAMES).getSWTWidget();
+		event.display = Display.getDisplay();
+		event.type = SWT.MouseDoubleClick;
+		event.button = 1;
+		return event;
 	}
 
 }
